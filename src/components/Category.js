9365b925a0c17c02146a49bbe31fff57187f8b11
Kevin Egstorf <kevin.egstorf@gmail.com>
2018-03-27 22:44:32 +0200

    added sinon and tests with mocking server

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,31 +3,43 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Clue from './Clue';
 
-class Category extends Component {
+export class Category extends Component {
   constructor() {
     super();
 
     this.state = { clues: [] };
   }
+
   componentDidMount() {
     fetch(`http://jservice.io/api/clues?category=${this.props.category.id}`)
-    .then(response => response.json())
-    .then(json => this.setState({ clues: json }));
+      .then(response => response.json())
+      .then(json => this.setState({ clues: json }));
   }
 
   render() {
-    console.log('category', this.props);
+    // console.log('category props', this.props);
+
     return (
       <div>
-        <Link to='/'><h4>Home</h4></Link>
         <h2>{this.props.category.title}</h2>
         {
           this.state.clues.map(clue => {
             return (
-              <Clue key={clue.id } clue={clue} />
+              <Clue key={clue.id} clue={clue} />
             )
           })
         }
+      </div>
+    )
+  }
+}
+
+export class LinkedCategory extends Component {
+  render() {
+    return (
+      <div>
+        <Link className='link-home' to='/'><h4>Home</h4></Link>
+        <Category category={this.props.category} />
       </div>
     )
   }
@@ -37,4 +49,4 @@ function mapStateToProps(state) {
   return { category: state.category }
 }
 
-export default connect(mapStateToProps, null)(Category);
+export default connect(mapStateToProps, null)(LinkedCategory);
